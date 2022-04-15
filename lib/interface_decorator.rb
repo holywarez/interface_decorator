a6668acac346d99c89bcc005c8fa7e7808602862
Anatoly Lapshin <a.lapshin@3commas.io>
2022-04-15 17:36:49 +0400

    Initial commit

---
+++ lib/interface_decorator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "interface_decorator/version"
+
+module InterfaceDecorator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/interface_decorator/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module InterfaceDecorator
+  VERSION = "0.1.0"
+end
